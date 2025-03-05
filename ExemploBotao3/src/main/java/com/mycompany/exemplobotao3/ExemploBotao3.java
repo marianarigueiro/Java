d0792fcbd9faad97f07263b6ae5d06e9c615553a
Mariana de O. Rigueiro <166918439+marianarigueiro@users.noreply.github.com>
2025-03-05 15:43:28 -0300

    Add files via upload

---
+++ ExemploBotao3/src/main/java/com/mycompany/exemplobotao3/ExemploBotao3.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exemplobotao3;
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+/**
+ *
+ * @author Yo
+ */
+public class ExemploBotao3 extends JFrame{
+    JButton botao1, botao2, botao3, botao4;
+    
+    public ExemploBotao3(){
+        super("Exemplo com JButton3");
+        Container tela = getContentPane();
+        setLayout(null);
+        
+        botao1 = new JButton("Procurar");
+        botao2 = new JButton("Voltar>>");
+        botao3 = new JButton("Próximo>>");
+        botao4 = new JButton("Abrir");
+        
+        botao1.setBounds(50,20,100,20);
+        botao2.setBounds(50,60,100,20);
+        botao3.setBounds(50,100,100,20);
+        botao4.setBounds(50,140,100,20);
+        
+        botao1.setBackground(Color.yellow);
+        botao2.setBackground(Color.red);
+        botao3.setBackground(Color.blue);
+        botao4.setBackground(Color.white);
+        
+        tela.add(botao1);
+        tela.add(botao2);
+        tela.add(botao3);
+        tela.add(botao4);
+        
+        setSize(400, 250);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        ExemploBotao3 app = new ExemploBotao3();
+        app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
